7594a32c68667d6c36840ea60ff33f6b368f79d7
Jordhan <jordhan.rdz@gmail.com>
2020-09-23 18:09:14 -0300

    Finished EndGame screen

--- index.ts
+++ index.ts
@@ -27,17 +27,26 @@ interface Answer {
 // ----------------------------------APP STATS------------------------------
 let token = "";
 let quizzes: Quizz[] = [];
-
-// ----create quizz stats---
+// ---create quizz stats---
 let questions: Question[] = [];
 let numberOfQuestions = 1;
 let questionNode: HTMLElement[] = [];
 let levels: Level[] = [];
 let numberOfLevels = 1;
 let levelNode: HTMLElement[] = [];
-// ----single quizz stats----
+// ---single quizz stats---
 let currentQuestion = 1;
 let acertos = 0;
+//---end game stats---
+let score = 0;
+let resultLevel: Level = {
+  title: "Voce eh praticamente um aluno de Hogwarst!",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit.Repellendus, qui! Doloremque, id? Nam perspiciatis suscipitlaboriosam sapiente eligendi obcaecati placeat minus, magni fugaomnis tempore incidunt ratione, mollitia rem facere?",
+  imageUrl:
+    "https://i.pinimg.com/564x/41/2d/83/412d83a778c691059de368b047c2a52f.jpg",
+  range: { minRange: 0, maxRange: 100 },
+};
 
 // ----------------------------------NODE && NODE LIST------------------------------
 const loginScreen = document.querySelector(".login-screen");
@@ -231,7 +240,8 @@ function selectAnswer(answer: HTMLElement, singleQuizz: Quizz) {
   const endGame = singleQuizz.data.questions.length < currentQuestion;
   if (endGame) {
     setTimeout(function () {
-      renderFromQuizzToEndGame();
+      calculateScoreAndLevel(singleQuizz);
+      renderFromQuizzToEndGame(singleQuizz);
     }, 2000);
   } else {
     setTimeout(function () {
@@ -252,6 +262,18 @@ function changeAnswers(whatToChange: string) {
     } else if (whatToChange === "removeClick") {
       // @ts-ignore
       // removeEventListener("click", teste);
+    }
+  }
+}
+
+// ---------------------------------END GAME SCREEN--------------------------------------
+
+function calculateScoreAndLevel(quizz: Quizz) {
+  score = Math.round((acertos / quizz.data.questions.length) * 100);
+  for (let level of quizz.data.levels) {
+    if (score >= level.range.minRange && score <= level.range.maxRange) {
+      resultLevel = level;
+      return;
     }
   }
 }
@@ -292,7 +314,8 @@ function renderFromQuizzesToQuizz(quizz: Quizz) {
   singleQuizzScreen?.classList.toggle("display-none");
 }
 
-function renderFromQuizzToEndGame() {
+function renderFromQuizzToEndGame(quizz: Quizz) {
+  renderResults(quizz);
   singleQuizzScreen?.classList.toggle("display-none");
   endGameScreen?.classList.toggle("display-none");
 }
@@ -408,6 +431,29 @@ function renderSingleQuestion(singleQuizz: Quizz) {
   }
   singleQuizzScreen?.insertAdjacentHTML("afterbegin", headerHtml);
   singleQuizzScreen?.insertAdjacentElement("beforeend", answersContainer);
+}
+
+function renderResults(quizz: Quizz) {
+  let html = `<h1>${quizz.title}</h1>
+  <header class="question-header">
+    <h3>Voce acertou ${acertos} de ${quizz.data.questions.length} perguntas ! Score: ${score}%</h3>
+  </header>
+  <div class="result-container">
+    <div class="result-text-container">
+      <h3>${resultLevel.title}</h3>
+      <p>
+        ${resultLevel.description}
+      </p>
+    </div>
+    <figure class="result-image-container">
+      <img
+        src="${resultLevel.imageUrl}"
+        alt=""
+      />
+    </figure>
+  </div>`;
+
+  endGameScreen?.insertAdjacentHTML("afterbegin", html);
 }
 
 // -------------------------------------HELPER FUNCTIONS----------------------------------------
