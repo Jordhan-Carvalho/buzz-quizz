d91d10caa9e84c452123d68b9cd65c3d51d6d743
Jordhan <jordhan.rdz@gmail.com>
2020-10-05 14:07:31 -0300

    Dev & Prod webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+const commonConfig = {
   entry: "./src/index.ts",
-  output: {
-    filename: "bundle.[contentHash].js",
-    path: path.resolve(__dirname, "docs"),
-  },
-  devtool: "inline-source-map",
   module: {
     rules: [
       {
@@ -21,4 +16,38 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [new HtmlWebpackPlugin({ template: "src/template.html" })],
+};
+
+const productionConfig = {
+  mode: "production",
+  output: {
+    filename: "bundle.[contentHash].js",
+    path: path.resolve(__dirname, "docs"),
+  },
+};
+
+const developmentConfig = {
+  mode: "development",
+  devtool: "inline-source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "docs"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "docs"),
+    port: 8000,
+  },
+};
+
+module.exports = (env) => {
+  console.log(env);
+  switch (env) {
+    case "development":
+      return { ...commonConfig, ...developmentConfig };
+    case "production":
+      return { ...commonConfig, ...productionConfig };
+    default:
+      throw new Error("No matching configuration was found!");
+  }
 };
